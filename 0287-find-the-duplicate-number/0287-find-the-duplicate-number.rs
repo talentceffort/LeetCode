33840a62415c507ab2452cbf2e782544d2f52576
talentceffort <47441575+talentceffort@users.noreply.github.com>
2023-09-19 15:47:56 +0900

    Time: 25 ms (7.62%), Space: 4.4 MB (7.62%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.rs
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.rs
@@ -1,21 +1,15 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 impl Solution {
     pub fn find_duplicate(nums: Vec<i32>) -> i32 {
-        let mut num_count = HashMap::new();
-
-        let mut result = 0;
-
-        for num in nums.iter() {
-            let count = num_count.entry(num).or_insert(0);
-            *count += 1;
-
-            if *count > 1 {
-                result = *num;
-                break
+        // HashSet을 이용한 풀이
+        let mut seen = HashSet::new();
+        for &num in nums.iter() {
+            if seen.contains(&num) {
+                return num;
             }
+            seen.insert(num);
         }
-
-        result
+        -1  // Just to satisfy the compiler, this should never be reached
     }
 }
