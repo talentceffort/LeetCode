b7a3cd6f83d37d28084b8cfee96515220b2987ce
talentceffort <47441575+talentceffort@users.noreply.github.com>
2023-10-11 10:39:32 +0900

    Time: 1 ms (78.37%), Space: 2.5 MB (22.70%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.rs
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.rs
@@ -1,20 +1,9 @@
 impl Solution {
     pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        if let Ok(index) = nums.binary_search(&target) {
+            return vec![nums.partition_point(|&i| i < target) as i32, nums.partition_point(|&i| i <= target) as i32 - 1];
+        } 
+        return vec![-1, -1];
         
-        
-        if nums.len() == 0 {
-            return vec![-1, -1];
-        }
-        
-        let a = nums.partition_point(|x| x < &target);
-        let b = nums.partition_point(|x| x <= &target);
-        
-        
-        if a > nums.len() - 1 || nums[a] != target {
-            return vec![-1, -1];
-        }
-        
-        
-        vec![a as i32, (b - 1) as i32]
     }
 }
