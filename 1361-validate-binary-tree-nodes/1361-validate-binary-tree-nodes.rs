11b324d3f48f68239b036b9efdee6a371fcba1c2
talentceffort <47441575+talentceffort@users.noreply.github.com>
2023-10-17 12:48:54 +0900

    Time: 17 ms (100.00%), Space: 2.5 MB (100.00%) - LeetHub

---
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.rs
@@ -0,0 +1,55 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn validate_binary_tree_nodes(n: i32, left_child: Vec<i32>, right_child: Vec<i32>) -> bool {
+        let root = find_root(n, &left_child, &right_child);
+        
+        if root == -1 {
+            return false;
+        }
+
+        let mut seen = HashSet::new();
+        let mut queue = VecDeque::new();
+
+        seen.insert(root);
+        queue.push_back(root);
+
+        while !queue.is_empty() {
+            let node = queue.pop_back().unwrap() as usize;
+            let children = vec![left_child[node], right_child[node]];
+
+            for child in children {
+                if child != -1 {
+                    let value = child;
+                    if seen.contains(&value) {
+                        return false;
+                    }
+                    queue.push_back(value);
+                    seen.insert(value);
+                }
+            }
+        }
+
+        return seen.len() == n as usize;
+
+        fn find_root(n: i32, left_child: &Vec<i32>, right_child: &Vec<i32>) -> i32 {
+            let mut children = HashSet::new();
+
+            for node in left_child {
+                children.insert(node);
+            }
+
+            for node in right_child {
+                children.insert(node);
+            }
+
+            for i in 0..n {
+                if !children.contains(&i) {
+                    return i;
+                }
+            }
+            -1
+        }
+    }
+}
